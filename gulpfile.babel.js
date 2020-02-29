1141ca9a3581a4f3fad53d08f5e20300f5ddbdef
Younggie Hong <younggie@gmail.com>
2020-02-29 15:37:54 +0900

    style.min.css file generate

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,8 +9,18 @@ import miniCSS from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
 import ghPages from "gulp-gh-pages";
+import postcss from "gulp-postcss";
+import tailwincss from "tailwindcss";
+import purgecss from "@fullhuman/postcss-purgecss";
+import rename from "gulp-rename";
 
 sass.compiler = require("node-sass");
+
+var postcssPlugins = [
+  purgecss({
+    content: ["src/**/*.html"]
+  })
+];
 
 const routes = {
   pug: {
@@ -42,7 +52,8 @@ const pug = () =>
 
 const clean = () => del(["build/", ".publish"]);
 
-const webserver = () => gulp.src("build").pipe(ws({ livereload: true }));
+//const webserver = () => gulp.src("build").pipe(ws({ livereload: true }));
+const webserver = () => gulp.src("src").pipe(ws({ livereload: true }));
 
 const img = () =>
   gulp
@@ -58,6 +69,22 @@ const styles = () =>
     .pipe(miniCSS())
     .pipe(gulp.dest(routes.scss.dest));
 
+const tailwind_minify = () =>
+  gulp
+    //.src("src/css/style.css")
+    .src("src/css/postcss/style.css")
+    .pipe(miniCSS())
+    .pipe(gulp.dest("src/css/minify"));
+
+//tailwindcss 에서 사용한 것만 추출해서 압축함
+const tailwind_postcss = () =>
+  gulp
+    .src("src/css/style.css")
+    .pipe(postcss(postcssPlugins))
+    .pipe(miniCSS())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("src/css"));
+
 const js = () =>
   gulp
     .src(routes.js.src)
@@ -71,12 +98,14 @@ const js = () =>
     )
     .pipe(gulp.dest(routes.js.dest));
 
-const gh = () => gulp.src("build/**/*").pipe(ghPages());
+//const gh = () => gulp.src("build/**/*").pipe(ghPages());
+const gh = () => gulp.src("src/**/*").pipe(ghPages());
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
   gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js);
   gulp.watch(routes.js.watch, js);
 };
 
@@ -90,3 +119,6 @@ export const build = gulp.series([prepare, assets]);
 export const dev = gulp.series([build, live]);
 //export const deploy = gulp.series([build, gh, clean]);
 export const deploy = gulp.series([build, gh]);
+export const myzip = gulp.series([tailwind_minify]);
+export const tailwind_use_only = gulp.series([tailwind_postcss]);
+export const run_server = gulp.series([live]);
